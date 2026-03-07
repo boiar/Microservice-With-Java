6fc6c7cba114d90ff40f82addd7e07ddf7808c21
boiar <kero2000boiar410@gmail.com>
2026-03-07 05:13:50 +0200

    init product service

--- services/customer/src/main/java/org/boiar/customer/handler/GlobalExceptionHandler.java
+++ services/customer/src/main/java/org/boiar/customer/handler/GlobalExceptionHandler.java
@@ -7,8 +7,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.lang.reflect.Field;
 import java.util.HashMap;
 
 @RestControllerAdvice
