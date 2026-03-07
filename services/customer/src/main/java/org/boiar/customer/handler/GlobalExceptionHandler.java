f5400803e052369fe161ef36042180e36a05d397
boiar <kero2000boiar410@gmail.com>
2026-03-07 04:53:18 +0200

    alter GlobalExceptionHandler in customer service

--- services/customer/src/main/java/org/boiar/customer/handler/GlobalExceptionHandler.java
+++ services/customer/src/main/java/org/boiar/customer/handler/GlobalExceptionHandler.java
@@ -1,16 +1,38 @@
 package org.boiar.customer.handler;
 
+import org.boiar.customer.exception.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<String> handle(CustomerNotFoundException exp) {
         return ResponseEntity
                 .badRequest()
-                .body(ex.getMessage());
+                .body(exp.getMessage());
+    }
+
+    // for req validation Exception
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException exp) {
+        var errors = new HashMap<String, String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var fieldName = ((FieldError) error).getField();
+                    var errorMsg = error.getDefaultMessage();
+                    errors.put(fieldName, errorMsg);
+                });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errors));
+
     }
 }
